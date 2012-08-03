ed2fb64d06fb9e2675c4d68218d6c3cf0101b2a1
Chris Gilmer <chris.gilmer@gmail.com>
2012-08-03 13:25:45 -0700

    Cleaned up the makehome script

--- makehome.sh
+++ makehome.sh
@@ -1,26 +1,31 @@
 #! /bin/bash
 
-# Link all the dotfiles
-ln -s .ackrc ~/.ackrc
-ln -s .bash_profile ~/.bash_profile
-ln -s .bashrc ~/.bashrc
-ln -s .gitconfig ~/.gitconfig
-ln -s .gitignore_global ~/.gitignore_global
-ln -s .hgrc ~/.hgrc
-ln -s .inputrc ~/.inputrc
-ln -s .ipython ~/.ipython
-ln -s .irbrc ~/.irbrc
-ln -s .screenrc ~/.screenrc
-ln -s .vim ~/.vim
-ln -s .vimrc ~/.vimrc
-
 # Get the submodules
 git submodule init
 git submodule update
 
-# Link the bin directory
+# Change to the home directory
 HOMEDIR=`pwd`
 cd ~/
-ln -s $HOMEDIR/bin .
 
+# Link all the files and dirs
+DOTFILES=(
+    ".ackrc"
+    ".bash_profile"
+    ".bashrc"
+    ".gitconfig"
+    ".gitignore_global"
+    ".hgrc"
+    ".inputrc"
+    ".ipython"
+    ".irbrc"
+    ".screenrc"
+    ".vim"
+    ".vimrc"
+    "bin")
 
+for dot in ${DOTFILES[@]}; do
+    ln -s $HOMEDIR/$dot .;
+done;
+
+cd $HOMEDIR
